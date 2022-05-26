4f8995f5608d6ce46e1fc7239de33b0956655c95
MickhailP <mich_p@inbox.ru>
2022-05-26 20:51:04 +0300

    Initial Commit

---
+++ WeatherAround/WeatherAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAroundApp.swift
+//  WeatherAround
+//
+//  Created by Миша Перевозчиков on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
